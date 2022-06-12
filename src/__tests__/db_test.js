6072a515afd6ac82152537be2420887dd3f86c63
Vishal Parikh <vparikh1@gmail.com>
2022-06-11 22:41:04 -0500

    Refactor tests

--- src/__tests__/db_test.js
+++ src/__tests__/db_test.js
@@ -1,5 +1,9 @@
 import { wbDb } from '../components/wb_db';
-import { initializeTestEnvironment } from "@firebase/rules-unit-testing";
+import {
+  assertFails,
+  assertSucceeds,
+  initializeTestEnvironment
+} from "@firebase/rules-unit-testing";
 import {
   get, set, ref, child
 } from "firebase/database";
@@ -20,36 +24,48 @@ async function dbSetup(){
 }
 
 test('users collection access', async () => {
-  expect.assertions(2);
   const env = await dbSetup();
 
-  await env.withSecurityRulesDisabled(async context => {
-    await context.database().ref('users/foobar4').set({ foo: 'bar' });
-  });
-
+  // await env.withSecurityRulesDisabled(async context => {
+  //   await context.database().ref('users/foobar4').set({ foo: 'bar' });
+  // });
 
   // unauthed user cannot write to users
   const unauthedDb = env.unauthenticatedContext().database();
-  let e;
-  try {
-    await unauthedDb.ref('users/unauth').set({bar:true});
-  }catch(_e){
-    //expect an error
-    e = _e;
-  }
-  expect(e.toString()).toBe('Error: PERMISSION_DENIED: Permission denied');
+  await assertFails(unauthedDb.ref('users/unauth').set({bar:true}));
 
   // user should be able to write to their own path
   const authedDb = env.authenticatedContext('bob').database();
-  await authedDb.ref('users/bob').update({bar:true});
+  await assertFails(authedDb.ref('users/bob').update({bar:true}));
   // If it didn't throw an exception then we're good
 
+  // Test that the data is there
+  // await env.withSecurityRulesDisabled(async context => {
+  //   await context.database().ref('users/bob').set({ foo: 'bar' });
+  // });
+
   // user should not be able to write to others paths
-  try {
-    await authedDb.ref('users/sally').set({bar:true});
-  }catch(_e){
-    //expect an error
-    e = _e;
-  }
-  expect(e.toString()).toBe('Error: PERMISSION_DENIED: Permission denied');
+  // TODO: test other paths?
+    await assertFails(authedDb.ref('users/sally').set({bar:true}));
 });
+
+//test('write to followsUser collection'), async() => {
+//  expect.assertions(2);
+//  const env = await dbSetup();
+
+//  let path = 'followsUser/user1';
+//  let userId = 'bob'
+
+//  //create an entry for user1 with a follower user2
+//  await env.withSecurityRulesDisabled(async context => {
+//    await context.database().ref(path).set({ user2: 0 });
+//  });
+
+//  const authedDb = env.authenticatedContext(userId).database();
+
+//  //should be able to set self as follower of another user
+//  let packet = {};
+//  packet[userId] = 0;
+//  await authedDb.ref(path).update(packet);
+
+//}
