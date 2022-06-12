e8dcf33638acd9e2dfc471422b22af537258dfad
Vishal Parikh <vparikh1@gmail.com>
2022-06-11 22:22:29 -0500

    refactor tests

--- src/__tests__/db_test.js
+++ src/__tests__/db_test.js
@@ -6,22 +6,6 @@ import {
 import { readFileSync, createWriteStream } from 'fs';
 
 
-const firebaseConfig = {
-  apiKey: 'key',
-  authDomain: 'domain',
-  projectId:'wordbird-674fc',
-  storageBucket: 'bucket',
-  messagingSenderId: 'id',
-  appId: 'appId',
-  measurementId: 'measurementid',
-  databaseURL: 'https://wbtest.firebaseio.com'
-};
-
-function setup(cb){
-  let db = new wbDb(firebaseConfig, {local:true, authCallBack:cb});
-  return db;
-}
-
 async function dbSetup(){
   const env = await initializeTestEnvironment({
     projectId: 'wbtest2',
@@ -35,24 +19,6 @@ async function dbSetup(){
   return env;
 }
 
-it('should exist', () => {
-  const db = setup();
-  expect(true).toBeTruthy();
-});
-
-it('should auth', done => {
-  console.log('test start');
-  const db = setup(profile =>{
-    console.log('got callback');
-    // This is the callback that is called on successful auth
-    expect(true).toBeTruthy();
-    expect(profile).not.toBeNull();
-    done(); // you have to call this to signal the test is over or it will time out
-  });
-
-  db.signIn();
-});
-
 test('users collection access', async () => {
   expect.assertions(2);
   const env = await dbSetup();
@@ -64,12 +30,14 @@ test('users collection access', async () => {
 
   // unauthed user cannot write to users
   const unauthedDb = env.unauthenticatedContext().database();
+  let e;
   try {
     await unauthedDb.ref('users/unauth').set({bar:true});
-  }catch(e){
+  }catch(_e){
     //expect an error
-    expect(e.toString()).toBe('Error: PERMISSION_DENIED: Permission denied');
+    e = _e;
   }
+  expect(e.toString()).toBe('Error: PERMISSION_DENIED: Permission denied');
 
   // user should be able to write to their own path
   const authedDb = env.authenticatedContext('bob').database();
@@ -79,8 +47,9 @@ test('users collection access', async () => {
   // user should not be able to write to others paths
   try {
     await authedDb.ref('users/sally').set({bar:true});
-  }catch(e){
+  }catch(_e){
     //expect an error
-    expect(e.toString()).toBe('Error: PERMISSION_DENIED: Permission denied');
+    e = _e;
   }
+  expect(e.toString()).toBe('Error: PERMISSION_DENIED: Permission denied');
 });
--- src/components/wb_db.test.js
+++ src/components/wb_db.test.js
@@ -1,5 +1,37 @@
-import wbDb from './wb_db'
+import {wbDb} from './wb_db'
+
+const firebaseConfig = {
+  apiKey: 'key',
+  authDomain: 'domain',
+  projectId:'wordbird-674fc',
+  storageBucket: 'bucket',
+  messagingSenderId: 'id',
+  appId: 'appId',
+  measurementId: 'measurementid',
+  databaseURL: 'https://wbtest.firebaseio.com'
+};
+
+
+function setup(cb){
+  let db = new wbDb(firebaseConfig, {local:true, authCallBack:cb});
+  return db;
+}
 
 it('should exist', () => {
+  const db = setup();
   expect(true).toBeTruthy();
-})
+});
+
+it('should auth', done => {
+  console.log('test start');
+  const db = setup(profile =>{
+    console.log('got callback');
+    // This is the callback that is called on successful auth
+    expect(true).toBeTruthy();
+    expect(profile).not.toBeNull();
+    done(); // you have to call this to signal the test is over or it will time out
+  });
+
+  db.signIn();
+});
+
